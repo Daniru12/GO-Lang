fa181f7ded592a9824c918e30532247e87269813
Daniru-Punsith <punsithdaniru@gmail.com>
2025-08-25 23:07:17 +0530

    set database

--- CRUDwithGO/main.go
+++ CRUDwithGO/main.go
@@ -14,34 +14,30 @@ import (
 	"patricego/transport/endpoints"
 	"patricego/usecases"
 )
-
 func main() {
-	
-	dsn := "root:achintha2002@tcp(127.0.0.1:3306)/taskDB?parseTime=true&loc=Asia%2FColombo"
+
+	dsn := "root:Punsith12@tcp(127.0.0.1:3306)/taskDB?parseTime=true&loc=Asia%2FColombo"
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatalf("Failed to connect to DB: %v", err)
 	}
 	defer db.Close()
 
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Database is not reachable: %v", err)
+	}
+
+	fmt.Println(" Database connected successfully!")
+
 	
 	taskRepo := mysql.NewTaskRepository(db)
-
-	
 	taskUsecase := usecases.NewTaskUsecase(taskRepo)
-
-	
 	taskService := services.NewTaskService(taskUsecase)
-
-	
 	taskHandler := endpoints.NewTaskHandler(taskService)
 
-	
 	router := transport.NewRouter(taskHandler)
 
-	
-	fmt.Println("Server started at :8081")
+	fmt.Println(" Server started at :8081")
 	log.Fatal(http.ListenAndServe(":8081", router))
 }
-
-
